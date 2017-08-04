e7d0f647de03bd7ef991b528b29c3ca750fe41d3
Darrell Payne <d.anthony.payne@gmail.com>
2017-08-03 22:44:55 -0700

    Rotating backgrounds completed

---
+++ core/src/Clouds/Cloud.java
@@ -0,0 +1,9 @@
+package Clouds;
+
+/**
+ * Created by Darrell Payne on 8/3/17.
+ */
+
+public class Cloud {
+    
+}
--- core/src/Scenes/Gameplay.java
+++ core/src/Scenes/Gameplay.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.sanctuaryofdarkness.jackthegiant.GameMain;
+
+import Helpers.GameInfo;
 
 /**
  * Created by Darrell Payne on 8/3/17.
@@ -19,23 +22,48 @@ public class Gameplay implements Screen {
     private Sprite[] bgs; // Array of backgrounds
     private OrthographicCamera mainCamera;
     private Viewport gameViewPort;
+    private float lastYPosition;
 
     public Gameplay(GameMain game){
         this.game = game;
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH,GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH / 2f,GameInfo.HEIGHT / 2, 0);
+        gameViewPort = new StretchViewport(GameInfo.WIDTH,GameInfo.HEIGHT);
+
         createBackgrounds();
     }
 
-    void createBackgrounds(){
+    void update(float dt){
+        moveCamera();
+        checkBackgroundsOutOfBounds();
+    }
+
+    private void moveCamera(){
+        mainCamera.position.y -= 2;
+    }
+
+    private void createBackgrounds(){
         bgs = new Sprite[3]; // 3 entries will be in array
         for (int i = 0; i < bgs.length; i++){
             bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
             bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
+            lastYPosition = Math.abs(bgs[i].getY());
         }
     }
 
-    void drawBackgrounds(){
+    private void drawBackgrounds(){
         for (int i = 0; i < bgs.length; i++){
             game.getBatch().draw(bgs[i], bgs[i].getX(),bgs[i].getY());
+        }
+    }
+
+    private void checkBackgroundsOutOfBounds() {
+        for (int i = 0; i < bgs.length; i++){
+            if ((bgs[i].getY() - bgs[i].getHeight() / 2f - 5) > mainCamera.position.y){
+                float newPosition = bgs[i].getHeight() + lastYPosition;
+                bgs[i].setPosition(0,-newPosition);
+                lastYPosition = Math.abs(newPosition);
+            }
         }
     }
 
@@ -47,12 +75,17 @@ public class Gameplay implements Screen {
     @Override
     public void render(float delta) {
 
+        update(delta);
+
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.getBatch().begin();
         drawBackgrounds();
         game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
 
     }
 
